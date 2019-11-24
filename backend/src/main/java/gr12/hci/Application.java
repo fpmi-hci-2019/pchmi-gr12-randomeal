e66e373cfe23320dfbde92bad498bb6e45757026
Songsterr99 <mr.songsterr@mail.ru>
2019-11-24 20:39:53 +0300

    Deployment file is added

--- backend/src/main/java/gr12/hci/Application.java
+++ backend/src/main/java/gr12/hci/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        int a = 10;
         SpringApplication.run(Application.class, args);
     }
 }
